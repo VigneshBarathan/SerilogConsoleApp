dc96cf9c9f32141fc92bc80e0ebe2f1dcf47b03e
Vignesh Barathan <vignesh.barathan@sisystems.com>
2022-01-09 19:58:18 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Sinks;
+using Serilog.Sinks.File;
+
+namespace SerilogConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.File("logfile.log", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            Log.Debug("Starting up");
+            Log.Debug("Shutting down");
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
